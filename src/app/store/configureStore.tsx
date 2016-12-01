f99f9e9c03bcea2ed45a3461e9ad354f323b26a1
wangkechun <hi@hi-hi.cn>
2016-12-01 11:48:38 +0800

    init

---
+++ src/app/store/configureStore.tsx
@@ -0,0 +1,17 @@
+/// <reference path="../../../typings/index.d.ts" />
+
+import {createStore} from 'redux';
+import rootReducer from '../reducers/index';
+
+declare var module: any;
+export default function configureStore(initialState: any) {
+  const store = createStore(rootReducer, initialState);
+  if (module.hot) {
+    // enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = rootReducer;
+      store.replaceReducer(nextReducer);
+    });
+  }
+  return store;
+}
